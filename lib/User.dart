78293970f41e20ffe1d5e67db9546c50f773f214
meEdilson3012 <istmeitchon@gmail.com>
2025-06-20 17:25:39 +0000

    estou cansado

--- lib/User.dart
+++ lib/User.dart
@@ -12,6 +12,8 @@ class User extends StatefulWidget {
 class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
+     var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -20,10 +22,11 @@ class _UserState extends State<User> {
       ),
 
       body: Container(
-        padding: EdgeInsets.all(20),
+
         child: Column(
           children: [
             Container(
+                      
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
@@ -47,37 +50,84 @@ class _UserState extends State<User> {
             ),
 
             SizedBox(height: 30),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Componentes().texto(
-                "Informações Pessoais",
-                22,
-                true,
-                Colors.black,
+            
+            
+            Container(
+              height: 550,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 247, 247, 247),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), 
+                  bottomLeft: Radius.circular(20)
+                  )
               ),
-            ),
-            SizedBox(height: 20),
-            Column(
-              children: [
-                Container(
+              child: Column(
+                children: [
                   
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 243, 243, 243),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.person),
-                      SizedBox(width: 20,),
-                       Componentes().texto("Edilson Cardoso", 20, true, Colors.black),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Icon(Icons.edit))
-                    ],
-                  ),
+                  Align(
+                alignment: Alignment.topLeft,
+                child: Componentes().texto(
+                  "Informações Pessoais",
+                  22,
+                  true,
+                  Colors.black,
                 ),
-              ],
+              ),SizedBox(height: 20),
+                  Container(
+                    width: width,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 3, 148, 15),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.person, color: Colors.white,),
+                        SizedBox(width: 20,),
+                         Componentes().texto("Edilson Cardoso", 20, true, Colors.white),
+                        
+                      ],
+                    ),
+                  ),
+                  SizedBox(height:  20,),
+                  Container(
+                    width: width,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 3, 148, 15),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone ,color: Colors.white,),
+                        SizedBox(width: 20,),
+                         Componentes().texto("955 26 52 60", 20, true, Colors.white),
+                        
+                      ],
+                    ),
+                  ),
+                  SizedBox(height:  20,),
+                  Container(
+                    width: width,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 3, 148, 15),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.location_city,
+                        color: Colors.white,
+                        ),
+                        SizedBox(width: 20,),
+                         Componentes().texto("Bissau,Cuntum", 20, true, const Color.fromARGB(255, 255, 255, 255)),
+                        
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
